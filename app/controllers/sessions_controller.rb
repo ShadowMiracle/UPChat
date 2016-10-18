898f7b7367a8cd8629db193196886f2c280bcb5a
Nguyen Hoang Tu <nguyenhoangtu91@gmail.com>
2016-10-18 18:00:44 +0700

    User can login user email and password

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @user = User.find_by_email(params[:email])
+      # If user password is correct
+      if @user.authenticate(params[:password])
+        # Add user_id to session
+        session[:user_id] = @user.id
+        flash[:success] = "Login successfully"
+        redirect_to root_path
+      else
+        flash.now[:error] = "Incorrect password"
+        render 'new'
+      end
+    else
+      flash.now[:error] = "Users not found"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully. Come back again"
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
 
+  resources :sessions, only: [:new, :create]
+  delete 'logout' => "sessions#destroy"
+
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
